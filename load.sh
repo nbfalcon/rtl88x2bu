b0a490fe6dc0242e530f8e7fc401ba33ceef1397
Nikita Bloshchanevich <nikblos@outlook.com>
2024-06-14 21:14:23 +0200

    Compile on latest kernel: use strlcpy; also: usbdrv.drvwrap

--- load.sh
+++ load.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 make -j8
+
+# Remove normal driver
+sudo rmmod rtw88_8822bu
+sudo rmmod rtw88_8822b
+
+# Insert our custom driver
 sudo rmmod ./88x2bu.ko
 sudo insmod ./88x2bu.ko rtw_power_mgnt=0 rtw_ips_mode=0 # rtw_monitor_fcsfail=1
--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -358,11 +358,7 @@ struct rtw_usb_drv usb_drv = {
 	.usbdrv.reset_resume   = rtw_resume,
 #endif
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 19))
-	.usbdrv.drvwrap.driver.shutdown = rtw_dev_shutdown,
-#else
 	.usbdrv.driver.shutdown = rtw_dev_shutdown,
-#endif
 };
 
 static inline int RT_usb_endpoint_dir_in(const struct usb_endpoint_descriptor *epd)
